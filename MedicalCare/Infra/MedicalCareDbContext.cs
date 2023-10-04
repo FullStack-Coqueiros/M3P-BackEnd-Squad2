1023da4f4817eab5c22f4a6414318da3746bf84c
Erico <ericoam.mussulin5@gmail.com>
2023-10-04 08:51:31 -0300

    Configuração do arquivo dbContext

--- MedicalCare/Infra/MedicalCareDbContext.cs
+++ MedicalCare/Infra/MedicalCareDbContext.cs
@@ -1,6 +1,20 @@
-﻿namespace MedicalCare.Infra
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MedicalCare.Infra
 {
-    public class MedicalCareDbContext
+    public class MedicalCareDbContext : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=DESKTOP-BG5E4QK\\SQLEXPRESS;Database=LabMedicineBd;Trusted_Connection=True;TrustServerCertificate=True;MultipleActiveResultSets=true";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.ApplyConfiguration(new EstudioConfiguration());
+        //    modelBuilder.ApplyConfiguration(new JogoConfiguration());
+        //}
+
     }
 }
