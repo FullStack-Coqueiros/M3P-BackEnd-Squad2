b2ecaa46ed746000cbe9a58497a2c3e117cd21cf
Erico <ericoam.mussulin5@gmail.com>
2023-10-03 22:05:45 -0300

    Criação da classe base PessoaModel e extração de algumas propriedades de PacienteModel e postas em PessoaModel para herdar.

--- MedicalCare/Models/PacienteModel.cs
+++ MedicalCare/Models/PacienteModel.cs
@@ -1,14 +1,10 @@
 ﻿namespace MedicalCare.Models
 {
-    public class PacienteModel
+    public class PacienteModel : PessoaModel
     {
-        public string NomeCompleto { get; set; }
-        public string Genero { get; set; }
         public DateTime DataDeNascimento { get; set; }
-        public string Cpf { get; set; }
         public string Rg { get; set; }
         public string EstadoCivil { get; set; }
-        public string Email { get; set; }
         public string Naturalidade { get; set; }
         public string ContatoDeEmergencia { get; set; }
         public List<string> Alergias { get; set; }
@@ -16,7 +12,6 @@
         public string Convenio { get; set; }
         public string NumeroDoConvenio { get; set; }
         public DateTime ValidadeDoConvenio { get; set; }
-        public bool StatusNoSistema { get; set; }
 
     }
 }
---
+++ MedicalCare/Models/PessoaModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MedicalCare.Models
+{
+    public class PessoaModel
+    {
+        public string NomeCompleto { get; set; }
+        public string Genero { get; set; }
+        public string Cpf { get; set; }
+        public string Email { get; set; }
+        public bool StatusNoSistema { get; set; }
+    }
+}
