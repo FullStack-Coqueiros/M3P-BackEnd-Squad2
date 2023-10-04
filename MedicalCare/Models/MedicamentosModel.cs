61d71bd1a76643c7169e8162a8fb286852a3c6c2
Vanessa <vanessascmonteiro@gmail.com>
2023-10-03 21:36:34 -0300

    criacaodaspropriedades

--- MedicalCare/Models/MedicamentosModel.cs
+++ MedicalCare/Models/MedicamentosModel.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -5,9 +6,29 @@ using System.Threading.Tasks;
 
 namespace MedicalCare.Models
 {
+    [Table("Medicmantos")]
     public class MedicamentosModel
     {
+        [Maxlenght(100), Minilenght(5)]
         public string NomeDoMedicamento {get; set;}
-        
+        public DateTime {get; set;}
+
+        public DateTime.Now {get; set;}
+
+        public string Tipo {get;set;}
+
+        public string Opcoes {get; set}
+
+        public number Quantidade {get; set}
+
+        public string Unidade {get; set}
+
+        public string Observacoes {get; set}
+
+        public bool StatusDoSistema {get; set}
+
+        public number PacienteId {get; set}
+
+        public number UsuarioId {get; set}
     }
 }
