1b9d2d1edd221ad0d5d79aee4d8d95049ea5482b
Erico <ericoam.mussulin5@gmail.com>
2023-10-03 22:59:03 -0300

    Inseridas propriedades de exercicioModel

--- MedicalCare/Models/ExercicioModel.cs
+++ MedicalCare/Models/ExercicioModel.cs
@@ -2,5 +2,17 @@
 {
     public class ExercicioModel
     {
+        //Creio que será uma relação n x n, visto que um tipo de exercicio pode estar presente em varios prontuarios,
+        //e um paciente pode ter varios tipos de exercicios prescritos
+
+        public string NomeDaSerieDeExercicios { get; set; }
+        public DateOnly Data { get; set; }
+        public TimeOnly Hora { get; set; }
+        public string Tipo { get; set; } //Possível enum
+        public int QuantidadePorSemana { get; set; }
+        public string Descricao { get; set; }
+        public bool StatusNoSistema { get; set; }
+        public int UsuarioId { get; set; } 
+        public int PacienteId { get; set; } // Possivel Icollection, pois podem haver varios
     }
 }
