812bb008193b666936a69a63325d8510e972b094
Erico <ericoam.mussulin5@gmail.com>
2023-10-04 08:57:04 -0300

    Configurando tabela exercicios

--- MedicalCare/Infra/Configurations/ExercicioConfiguration.cs
+++ MedicalCare/Infra/Configurations/ExercicioConfiguration.cs
@@ -1,6 +1,16 @@
-﻿namespace MedicalCare.Infra.Configurations
+﻿using MedicalCare.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MedicalCare.Infra.Configurations
 {
-    public class ExercicioConfiguration
+    public class ExercicioConfiguration : IEntityTypeConfiguration<ExercicioModel>
     {
+        public void Configure(EntityTypeBuilder<ExercicioModel> builder)
+        {
+            builder.ToTable("Exercicios");
+            builder.HasKey(x => x.Id);
+            builder.Property(p => p.Id).UseIdentityColumn();
+        }
     }
 }
