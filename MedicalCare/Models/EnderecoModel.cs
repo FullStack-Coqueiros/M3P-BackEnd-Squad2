7a7efbfe50e06f8df0c223745fbc10c100d1e525
Erico <ericoam.mussulin5@gmail.com>
2023-10-03 22:18:50 -0300

    Criação de propriedades de endereçoModel

--- MedicalCare/Models/EnderecoModel.cs
+++ MedicalCare/Models/EnderecoModel.cs
@@ -2,6 +2,14 @@
 {
     public class EnderecoModel
     {
+        public string Cep { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string PontoDeReferencia { get; set; }
 
     }
 }
