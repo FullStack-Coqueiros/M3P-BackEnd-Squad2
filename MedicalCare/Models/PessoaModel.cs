5696f4f96a13fde6612c670810e09f5ae52108d6
Erico <ericoam.mussulin5@gmail.com>
2023-10-05 20:28:43 -0300

    Feitas as validações na PessoaModel

--- MedicalCare/Models/PessoaModel.cs
+++ MedicalCare/Models/PessoaModel.cs
@@ -1,12 +1,30 @@
-﻿namespace MedicalCare.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MedicalCare.Models
 {
     public class PessoaModel
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(64, MinimumLength = 8, ErrorMessage = "Deve possuir entre 8 e 64 caracteres.")]
+        [Column ("nome_completo")]
         public string NomeCompleto { get; set; }
-        public string Genero { get; set; }
+
+        [Required]
+        public string Genero { get; set; } // mudar para Enum
+
+        [Required(ErrorMessage = "CPF é obrigatório.")]
+        [StringLength(11, MinimumLength = 11, ErrorMessage = "CPF deve conter 11 caracteres.")]
         public string Cpf { get; set; }
+
+        [Required (ErrorMessage = "Email é obrigatório")]  
         public string Email { get; set; }
+
+        [Column("Status do Sistema")]
+        [Required(ErrorMessage = "O Status é obrigatório")]
         public bool StatusNoSistema { get; set; }
     }
 }
