dcd160770f1ea09c66745424c6b4b6a007ba074b
Erico <ericoam.mussulin5@gmail.com>
2023-10-05 19:58:42 -0300

    Criação dos DbSets.

--- MedicalCare/Infra/MedicalCareDbContext.cs
+++ MedicalCare/Infra/MedicalCareDbContext.cs
@@ -1,18 +1,22 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using MedicalCare.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace MedicalCare.Infra
 {
     public class MedicalCareDbContext : DbContext
     {
+        public DbSet<EnderecoModel> DbEndereco { get; set; }
+        public DbSet<ExercicioModel> DbExercicio { get; set; }
+        public DbSet<PacienteModel> DbPaciente { get; set; }
+
+        public MedicalCareDbContext(){ }
+        public MedicalCareDbContext(DbContextOptions<MedicalCareDbContext> options) : base(options) { }
+
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             var connectionString = "Server=DESKTOP-BG5E4QK\\SQLEXPRESS;Database=LabMedicineBd;Trusted_Connection=True;TrustServerCertificate=True;MultipleActiveResultSets=true";
             optionsBuilder.UseSqlServer(connectionString);
-        }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-
         }
 
 
