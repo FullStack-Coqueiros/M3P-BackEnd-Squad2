49f8e4552f58263874163eab369b713e6694462a
Lucas <carneirolucas88@gmail.com>
2023-10-06 19:13:19 -0300

    Cria ExameMapper

---
+++ MedicalCare/AutoMapper/ExameMapper.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using MedicalCare.Models;
+using MedicalCare.DTOs;
+
+namespace MedicalCare.AutoMapper
+{
+    public class ExameMapper : Profile
+    {
+        public ExameMapper()
+        {
+           
+            CreateMap<ExameDTO, ExameModel>();
+
+
+            CreateMap<ExameModel, ExameDTO>();
+        }
+    }
+}
