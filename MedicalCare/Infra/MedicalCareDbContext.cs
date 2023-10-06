691af999a6eb5b48f324389346bd639b9bc16bf5
Lucas <carneirolucas88@gmail.com>
2023-10-06 14:53:51 -0300

    configuração com o banco de dados

--- MedicalCare/Infra/MedicalCareDbContext.cs
+++ MedicalCare/Infra/MedicalCareDbContext.cs
@@ -5,22 +5,13 @@ namespace MedicalCare.Infra
 {
     public class MedicalCareDbContext : DbContext
     {
-        public DbSet<EnderecoModel> DbEndereco { get; set; }
-        public DbSet<ExercicioModel> DbExercicio { get; set; }
-        public DbSet<PacienteModel> DbPaciente { get; set; }
+        //public DbSet<EnderecoModel> DbEndereco { get; set; }
+        //public DbSet<ExercicioModel> DbExercicio { get; set; }
+        //public DbSet<PacienteModel> DbPaciente { get; set; }
         public DbSet<ExameModel> DbExame { get; set; }
         public DbSet<DietaModel> DbDieta { get; set; }
 
-        public MedicalCareDbContext(){ }
         public MedicalCareDbContext(DbContextOptions<MedicalCareDbContext> options) : base(options) { }
-
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            var connectionString = "Server=DESKTOP-BG5E4QK\\SQLEXPRESS;Database=LabMedicineBd;Trusted_Connection=True;TrustServerCertificate=True;MultipleActiveResultSets=true";
-            optionsBuilder.UseSqlServer(connectionString);
-        }
-
-
     }
+ 
 }
