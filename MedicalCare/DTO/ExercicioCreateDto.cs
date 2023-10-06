ea128818670cb76c98590d16583577c164bac5e9
Erico <ericoam.mussulin5@gmail.com>
2023-10-05 23:22:33 -0300

    Criadas dtos de exercicios.

---
+++ MedicalCare/DTO/ExercicioCreateDto.cs
@@ -0,0 +1,17 @@
+﻿using MedicalCare.Enums;
+
+namespace MedicalCare.DTO
+{
+    public class ExercicioCreateDto
+    {
+        public string NomeDaSerieDeExercicios { get; set; }
+        public string Data { get; set; }
+        public string Hora { get; set; }
+        public EtipoExercicio Tipo { get; set; }
+        public int QuantidadePorSemana { get; set; }
+        public string Descricao { get; set; }
+        public bool StatusNoSistema { get; set; }
+        public int UsuarioId { get; set; }
+        public int PacienteId { get; set; }
+    }
+}
