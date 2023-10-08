d40b2b280dcc69fa90eeb6b7402290fbe7e36480
Erico <ericoam.mussulin5@gmail.com>
2023-10-08 19:02:38 -0300

    Criado classe repository genérica e sua interface.

---
+++ MedicalCare/Services/EnderecoService.cs
@@ -0,0 +1,41 @@
+﻿using MedicalCare.Interfaces;
+using MedicalCare.Models;
+
+namespace MedicalCare.Services
+{
+    public class EnderecoService
+    {
+        private readonly IDietaRepository _dietaRepository;
+
+        public EnderecoService(IDietaRepository dietaRepository)
+        {
+            _dietaRepository = dietaRepository;
+        }
+
+        public async Task<IEnumerable<DietaModel>> GetAllEnderecosAsync()
+        {
+            return await _dietaRepository.GetAllDietasAsync();
+        }
+
+        public async Task<DietaModel> GetEnderecoByIdAsync(int id)
+        {
+            return await _dietaRepository.GetDietaByIdAsync(id);
+        }
+
+        public async Task<int> CreateEnderecoAsync(DietaModel dieta)
+        {
+            return await _dietaRepository.CreateDietaAsync(dieta);
+        }
+
+        public async Task<bool> UpdateDietaAsync(DietaModel dieta)
+        {
+            return await _dietaRepository.UpdateDietaAsync(dieta);
+        }
+
+        public async Task<bool> DeleteDietaAsync(int id)
+        {
+            return await _dietaRepository.DeleteDietaAsync(id);
+        }
+
+    }
+}
