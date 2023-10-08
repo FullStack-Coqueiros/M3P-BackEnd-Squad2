324f0cc73268e5932eb176275f1ca855f3385465
Lucas <carneirolucas88@gmail.com>
2023-10-07 22:49:21 -0300

    Cria ExameService

--- MedicalCare/Repositoryes/ExameRepository.cs
+++ MedicalCare/Repositoryes/ExameRepository.cs
@@ -1,6 +1,64 @@
-﻿namespace MedicalCare.Repositoryes
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MedicalCare.Infra;
+using MedicalCare.Models;
+
+public class ExameRepository
 {
-    public class ExameRepository
+    private readonly MedicalCareDbContext _context;
+
+    public ExameRepository(MedicalCareDbContext context)
     {
+        _context = context;
+    }
+
+    public ExameModel Create(ExameModel exame)
+    {
+        _context.DbExame.Add(exame);
+        _context.SaveChanges();
+        return exame;
+    }
+
+    public ExameModel Update(ExameModel exame)
+    {
+        var existingExame = _context.DbExame.Find(exame.Id);
+        if (existingExame != null)
+        {
+            // Atualizar os campos necessários
+            existingExame.NomeDoExame = exame.NomeDoExame;
+            existingExame.DataDoExame = exame.DataDoExame;
+            existingExame.HorarioDoExame = exame.HorarioDoExame;
+            // ...
+
+            _context.SaveChanges();
+
+            return existingExame;
+        }
+
+        return null; // Indica que o exame não foi encontrado
+    }
+
+    public ExameModel GetById(int id)
+    {
+        return _context.DbExame.Find(id);
+    }
+
+    public IEnumerable<ExameModel> GetAll()
+    {
+        return _context.DbExame.ToList();
+    }
+
+    public bool Delete(int id)
+    {
+        var exame = _context.DbExame.Find(id);
+        if (exame != null)
+        {
+            _context.DbExame.Remove(exame);
+            _context.SaveChanges();
+            return true;
+        }
+
+        return false; // Indica que o exame não foi encontrado
     }
 }
---
+++ MedicalCare/Services/ExameService.cs
@@ -0,0 +1,42 @@
+﻿using MedicalCare.Models;
+
+namespace MedicalCare.Services
+{
+
+    public class ExameService
+    {
+        private readonly ExameRepository _exameRepository;
+
+        public ExameService(ExameRepository exameRepository)
+        {
+            _exameRepository = exameRepository;
+        }
+
+        public ExameModel CreateExame(ExameModel exame)
+        {
+            // Adicionar lógica de validação se necessário
+            return _exameRepository.Create(exame);
+        }
+
+        public ExameModel UpdateExame(ExameModel exame)
+        {
+            // Adicionar lógica de validação se necessário
+            return _exameRepository.Update(exame);
+        }
+
+        public ExameModel GetExameById(int id)
+        {
+            return _exameRepository.GetById(id);
+        }
+
+        public IEnumerable<ExameModel> GetAllExames()
+        {
+            return _exameRepository.GetAll();
+        }
+
+        public bool DeleteExame(int id)
+        {
+            return _exameRepository.Delete(id);
+        }
+    }
+}
