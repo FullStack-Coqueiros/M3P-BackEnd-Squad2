77371284cdb49304fe246fba566758e1789c916f
Lucas <carneirolucas88@gmail.com>
2023-10-09 01:08:04 -0300

    removendo arquivos repositoryes

--- MedicalCare/Services/ExameService.cs
+++ MedicalCare/Services/ExameService.cs
@@ -1,40 +1,11 @@
 ﻿
-using MedicalCare.Models;
-using MedicalCare.Interfaces;  
 
-public class ExameService
+namespace MedicalCare.Services
 {
-    private readonly IExameRepository _exameRepository;  
-
-    public ExameService(IExameRepository exameRepository)
+    public class DietaService
     {
-        _exameRepository = exameRepository;
-    }
 
-    public ExameModel CreateExame(ExameModel exame)
-    {
-        //  lógica de validação se necessário
-        return _exameRepository.Create(exame);
-    }
 
-    public ExameModel UpdateExame(ExameModel exame)
-    {
-        //  lógica de validação se necessário
-        return _exameRepository.Update(exame);
-    }
 
-    public ExameModel GetExameById(int id)
-    {
-        return _exameRepository.GetById(id);
-    }
-
-    public IEnumerable<ExameModel> GetAllExames()
-    {
-        return _exameRepository.GetAll();
-    }
-
-    public bool DeleteExame(int id)
-    {
-        return _exameRepository.Delete(id);
     }
 }
