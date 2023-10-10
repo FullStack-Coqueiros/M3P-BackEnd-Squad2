474d0d0c493f1eff5804ec56e6f47cd8ba1655de
Lucas <carneirolucas88@gmail.com>
2023-10-10 18:35:06 -0300

    refaturando DietaService

--- MedicalCare/Interfaces/IDietaService.cs
+++ MedicalCare/Interfaces/IDietaService.cs
@@ -5,7 +5,6 @@ namespace MedicalCare.Interfaces
 {
     public interface IDietaService
     {
-      
         DietaGetDto CreateDieta(DietaCreateDto dieta);
         DietaGetDto UpdateDieta(DietaUpdateDto dieta);
         DietaGetDto GetById(int id);
--- MedicalCare/Services/DietaService.cs
+++ MedicalCare/Services/DietaService.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 
-
 namespace MedicalCare.Services
 {
     public class DietaService : IDietaService
