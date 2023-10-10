b3da3615217543e7449c5d0bd605c1cd0fd0669c
Erico <ericoam.mussulin5@gmail.com>
2023-10-09 21:16:18 -0300

    Criada service base de usuario.

---
+++ MedicalCare/Services/UsuarioService.cs
@@ -0,0 +1,41 @@
+﻿using MedicalCare.Interfaces;
+using MedicalCare.Models;
+
+namespace MedicalCare.Services
+{
+    public class UsuarioService
+    {
+        private readonly IRepository<UsuarioModel> _usuarioRepository;
+
+        public UsuarioService(IRepository<UsuarioModel> usuarioRepository)
+        {
+            _usuarioRepository = usuarioRepository;
+        }
+
+        public IEnumerable<UsuarioModel> GetAllEnderecos()
+        {
+            return _usuarioRepository.GetAll();
+        }
+
+        public UsuarioModel GetById(int id)
+        {
+            return _usuarioRepository.GetById(id);
+        }
+
+        public UsuarioModel CreateEndereco(UsuarioModel endereco)
+        {
+            return _usuarioRepository.Create(endereco);
+            //fazer mapper antes de retornar
+        }
+
+        public UsuarioModel UpdateEndereco(UsuarioModel endereco)
+        {
+            return _usuarioRepository.Update(endereco);
+        }
+
+        public bool DeleteEndereco(int id)
+        {
+            return _usuarioRepository.Delete(id);
+        }
+
+    }
