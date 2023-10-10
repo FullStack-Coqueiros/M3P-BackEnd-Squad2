331a8ecc5c301764c663fbbe339b9e206e49288e
Erico <ericoam.mussulin5@gmail.com>
2023-10-10 18:31:10 -0300

    Adicionadas propriedades em PacienteUpdateDto

--- MedicalCare/DTO/PacienteUpdateDto.cs
+++ MedicalCare/DTO/PacienteUpdateDto.cs
@@ -1,18 +1,25 @@
 ﻿using MedicalCare.Enums;
+using MedicalCare.Models;
 
 namespace MedicalCare.DTO
 {
     public class PacienteUpdateDto
     {
-        public string Email { get; set; }
-        public bool StatusNoSistema { get; set; }
+        public string NomeCompleto { get; set; }
+        public Egenero Genero { get; set; }
+        public DateTime DataDeNascimento { get; set; }
         public EestadoCivil EstadoCivil { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public string Naturalidade { get; set; }
         public string ContatoDeEmergencia { get; set; }
         public List<string> Alergias { get; set; }
         public List<string> CuidadosEspecificos { get; set; }
         public string Convenio { get; set; }
         public string NumeroDoConvenio { get; set; }
         public DateTime ValidadeDoConvenio { get; set; }
+        public EnderecoModel Endereco { get; set; }
+        public bool StatusNoSistema { get; set; }
     }
 }
 
