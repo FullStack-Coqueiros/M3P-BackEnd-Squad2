c5d3a4dddcdaf920a9099b6869fc333cae26d50e
Vanessa <vanessascmonteiro@gmail.com>
2023-10-11 20:46:05 -0300

    criacaoenumopcoesdemedicamentos

---
+++ MedicalCare/Enums/EOpcoesMedicamentos.cs
@@ -0,0 +1,15 @@
+namespace MedicalCare.Enums
+{
+    public enum EOpcoesMedicamentos
+    {
+        cápsula = 0,
+        comprimido = 1,
+        líquido = 2,
+        creme = 3,
+        gel = 4,
+        inalação = 5,
+        injeção = 6,
+        spray = 7
+
+    }
+}
