617fcf3bf31835feb52bf1df0f7941296eee8853
Lucas <carneirolucas88@gmail.com>
2023-10-10 21:21:38 -0300

    refaturando ExameService

--- MedicalCare/Services/ExameService.cs
+++ MedicalCare/Services/ExameService.cs
@@ -9,9 +9,62 @@ using Microsoft.EntityFrameworkCore;
 
 namespace MedicalCare.Services
 {
-    public class ExameService //: IExameService
+    public class ExameService : IExameService
     {
-      
+        private readonly IRepository<ExameModel> _exameRepository;
+        private readonly IMapper _mapper;
+
+        public ExameService(IMapper mapper, IRepository<ExameModel> exameRepository)
+        {
+            _mapper = mapper;
+            _exameRepository = exameRepository;
+        }
+
+        public IEnumerable<ExameGetDto> GetAllExames()
+        {
+            IEnumerable<ExameModel> exames = _exameRepository.GetAll();
+            IEnumerable<ExameGetDto> exameGet = _mapper.Map<IEnumerable<ExameGetDto>>(exames);
+            return exameGet;
+        }
+
+        public ExameGetDto GetById(int id)
+        {
+            ExameModel exame = _exameRepository.GetById(id);
+            ExameGetDto exameGetId = _mapper.Map<ExameGetDto>(exame);
+            return exameGetId;
+        }
+
+        public ExameGetDto CreateExame(ExameCreateDto exame)
+        {
+            ExameModel exameModel = _mapper.Map<ExameModel>(exame);
+            _exameRepository.Create(exameModel);
+            ExameGetDto exameGet = _mapper.Map<ExameGetDto>(exame);
+            return exameGet;
+        }
+
+        public ExameGetDto UpdateExame(ExameUpdateDto exame)
+        {
+            ExameModel exameModel = _mapper.Map<ExameModel>(exame);
+            _exameRepository.Update(exameModel);
+            ExameGetDto exameGet = _mapper.Map<ExameGetDto>(exame);
+            return exameGet;
+        }
+
+        public bool DeleteExame(int id)
+        {
+            bool remocao = _exameRepository.Delete(id);
+            if (remocao)
+            {
+                return true;
+            }
+            return false;
+        }
 
     }
 }
+
+
+
+
+
+
