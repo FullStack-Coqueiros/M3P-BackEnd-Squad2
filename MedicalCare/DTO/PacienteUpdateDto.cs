13a6c4cbe3847696bcfb0cd73df8398282421fc2
Erico <ericoam.mussulin5@gmail.com>
2023-10-15 10:27:55 -0300

    Remoção das propriedades CPF e RG de pacienteUpdateDto.

--- MedicalCare/DTO/PacienteUpdateDto.cs
+++ MedicalCare/DTO/PacienteUpdateDto.cs
@@ -6,8 +6,6 @@ namespace MedicalCare.DTO
     public class PacienteUpdateDto
     {
         public string NomeCompleto { get; set; }
-        public  string Cpf { get; set; }
-        public string Rg { get; set; }
         public Egenero Genero { get; set; }
         public DateTime DataDeNascimento { get; set; }
         public EestadoCivil EstadoCivil { get; set; }
