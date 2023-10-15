5d036e5322a2e8c4ee133129af0c0665db17d31a
Delson Magalhaes <dl_magalhaes@hotmail.com>
2023-10-15 17:53:43 -0300

    CriandoLogUpdateDTO

---
+++ MedicalCare/DTO/LogUpdateDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace MedicalCare.DTO
+{
+    public class LogUpdateDTO
+    {
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public string Dominio { get; set; }
+        public string Descricao { get; set; }
+    }
+}
