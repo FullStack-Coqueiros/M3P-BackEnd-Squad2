f7dc942c5436cb7a85c4f91f9e00dba2218a034c
Erico <ericoam.mussulin5@gmail.com>
2023-10-17 17:39:42 -0300

    Criado método gerarToken em autenticacaoService.

--- MedicalCare/Services/AutenticacaoService.cs
+++ MedicalCare/Services/AutenticacaoService.cs
@@ -2,17 +2,23 @@
 using MedicalCare.Interfaces;
 using MedicalCare.Models;
 using MedicalCare.Utils;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace MedicalCare.Services
 {
     public class AutenticacaoService : IAutenticacaoService
     {
         private readonly IUsuarioService _usuarioService;
+        private readonly string _chaveJwt;
 
 
-        public AutenticacaoService(IUsuarioService usuarioService)
+        public AutenticacaoService(IUsuarioService usuarioService, IConfiguration configuration)
         {
             _usuarioService = usuarioService;
+            _chaveJwt = configuration.GetSection("jwtTokenChave").Get<string>();
         }
 
         public bool Autenticar(LoginDto loginDto)
@@ -27,6 +33,30 @@ namespace MedicalCare.Services
                                                                                     // Essa verificação se dá se for levado em conta
                                                                                     // que ao criar um novo usuário a senha já seja
                                                                                     // salva com criptografia.
+        }
+
+        public string GerarToken (LoginDto loginDto)
+        {
+            UsuarioModel usuario = _usuarioService.GetByEmail(loginDto.Email);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_chaveJwt);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, usuario.Email),
+                    new Claim("Nome", usuario.NomeCompleto),
+                    new Claim("Id", usuario.Id.ToString()),
+                    new Claim(ClaimTypes.Role, usuario.Tipo.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddHours(4),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
         }
 
     }
