80a4d744815c060e60e8f37b5cc8d91576c03675
Vanessa <vanessascmonteiro@gmail.com>
2023-10-16 21:28:33 -0300

    criacaomedicamentocontroller

--- MedicalCare/Interfaces/IMedicamentoService.cs
+++ MedicalCare/Interfaces/IMedicamentoService.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using MedicalCare.DTO;
 
+
 namespace MedicalCare.Interfaces
 {
     public interface IMedicamentoService
     {
         MedicamentoGetDTO CreateMedicamento(MedicamentoCreateDTO medicamento);
-        MedicamentoGetDTO UpdateMedicamento(MedicamentoUpdateDTO medicamento);
+        MedicamentoGetDTO UpdateMedicamento(MedicamentoUpdateDTO medicamento, int id);
         MedicamentoGetDTO GetById (int id);
         IEnumerable<MedicamentoGetDTO> GetAllMedicamentos();
         bool DeleteMedicamento(int id);
