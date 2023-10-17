7d5de0638e5a1878098772e31ba4c825276a1c7b
Lucas <carneirolucas88@gmail.com>
2023-10-16 22:53:36 -0300

    validações DTOs entidades Dieta e Exame

--- MedicalCare/DTO/DietaUpdateDTO.cs
+++ MedicalCare/DTO/DietaUpdateDTO.cs
@@ -1,16 +1,35 @@
-﻿using MedicalCare.Enums;
+﻿using System;
+using MedicalCare.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace MedicalCare.DTO
 {
     public class DietaUpdateDto
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "O Nome da Dieta é obrigatório.")]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "O Nome da Dieta deve ter entre 5 e 100 caracteres.")]
         public string NomeDaDieta { get; set; }
+
         public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "A Data é obrigatória.")]
         public DateTime Data { get; set; }
+
+        [Required(ErrorMessage = "O Horário é obrigatório.")]
         public DateTime Horario { get; set; }
+
+        [Required(ErrorMessage = "O Tipo da Dieta é obrigatório.")]
+        [StringLength(32, ErrorMessage = "O Tipo da Dieta deve ter no máximo 32 caracteres.")]
         public ETipoDieta Tipo { get; set; }
+
+        public string DescricaoDaDietaExecutada { get; set; }
+
+        [Required(ErrorMessage = "O Id do Paciente é obrigatório.")]
         public int PacienteId { get; set; }
+
+        [Required(ErrorMessage = "O Id do Usuário é obrigatório.")]
         public int UsuarioId { get; set; }
 
     }
