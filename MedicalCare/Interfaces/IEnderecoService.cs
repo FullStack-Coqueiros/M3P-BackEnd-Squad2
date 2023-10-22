541f2668995cf67db0cca8871e356830c98f0c40
Erico <ericoam.mussulin5@gmail.com>
2023-10-21 23:00:25 -0300

    Alterado tipo de retorno e implementado adição de endereço em EnderecoService.CreateEndereco.

--- MedicalCare/Interfaces/IEnderecoService.cs
+++ MedicalCare/Interfaces/IEnderecoService.cs
@@ -1,12 +1,14 @@
-﻿using MedicalCare.Models;
+﻿using MedicalCare.DTO;
+using MedicalCare.Models;
 
 namespace MedicalCare.Interfaces
 {
     public interface IEnderecoService
     {
         IEnumerable<EnderecoModel> GetAllEnderecos();
-        EnderecoModel CreateEndereco(EnderecoModel endereco);
+        EnderecoGetDto CreateEndereco(EnderecoCreateDto endereco);
         EnderecoModel GetById(int id);
+        EnderecoGetDto GetByRelationship(PacienteModel relationship);
         EnderecoModel UpdateEndereco(EnderecoModel endereco);
         bool DeleteEndereco(int id);
 
