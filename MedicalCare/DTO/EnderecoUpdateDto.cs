af90c2ad58ce473682f9efb9f45b049bfe27dcac
Erico <ericoam.mussulin5@gmail.com>
2023-10-21 23:25:00 -0300

    Inseridas mensagens de erro nas dtos de endereço.

--- MedicalCare/DTO/EnderecoUpdateDto.cs
+++ MedicalCare/DTO/EnderecoUpdateDto.cs
@@ -4,28 +4,28 @@ namespace MedicalCare.DTO
 {
     public class EnderecoUpdateDto
     {
-        [Required, StringLength(8, MinimumLength = 8)]
+        [Required, StringLength(8, MinimumLength = 8, ErrorMessage = "Insira 8 caracteres.")]
         public string cep { get; set; }
 
-        [Required, StringLength(30, MinimumLength = 3)]
+        [Required, StringLength(30, MinimumLength = 3, ErrorMessage = "Insira entre 3 e 30 caracteres.")]
         public string localidade { get; set; }
 
-        [Required, StringLength(20, MinimumLength = 2)]
+        [Required, StringLength(20, MinimumLength = 2, ErrorMessage = "Insira entre 2 e 20 caracteres.")]
         public string uf { get; set; }
 
-        [Required, StringLength(50, MinimumLength = 3)]
+        [Required, StringLength(50, MinimumLength = 3, ErrorMessage = "Insira entre 3 e 50 caracteres.")]
         public string logradouro { get; set; }
 
         [Required]
         public string Numero { get; set; }
 
-        [Required, StringLength(40, MinimumLength = 3)]
+        [Required, StringLength(40, MinimumLength = 3, ErrorMessage = "Insira entre 3 e 40 caracteres.")]
         public string Complemento { get; set; }
 
-        [Required, StringLength(50, MinimumLength = 3)]
+        [Required, StringLength(50, MinimumLength = 3, ErrorMessage = "Insira entre 3 e 50 caracteres.")]
         public string bairro { get; set; }
 
-        [Required,StringLength(60, MinimumLength = 6)]
+        [Required, StringLength(60, MinimumLength = 6, ErrorMessage = "Insira entre 6 e 60 caracteres.")]
         public string PontoDeReferencia { get; set; }
     }
 }
