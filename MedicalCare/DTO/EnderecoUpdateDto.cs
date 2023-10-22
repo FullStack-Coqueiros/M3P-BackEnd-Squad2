e1c2ee51d13f9e21143ade2111a7c562fbc4dd55
Erico <ericoam.mussulin5@gmail.com>
2023-10-21 23:20:48 -0300

    Mudança n local das datannotations da model para a create e update.

--- MedicalCare/DTO/EnderecoUpdateDto.cs
+++ MedicalCare/DTO/EnderecoUpdateDto.cs
@@ -1,14 +1,31 @@
-﻿namespace MedicalCare.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MedicalCare.DTO
 {
     public class EnderecoUpdateDto
     {
-        public string Cep { get; set; }
-        public string Cidade { get; set; }
-        public string Estado { get; set; }
-        public string Logradouro { get; set; }
+        [Required, StringLength(8, MinimumLength = 8)]
+        public string cep { get; set; }
+
+        [Required, StringLength(30, MinimumLength = 3)]
+        public string localidade { get; set; }
+
+        [Required, StringLength(20, MinimumLength = 2)]
+        public string uf { get; set; }
+
+        [Required, StringLength(50, MinimumLength = 3)]
+        public string logradouro { get; set; }
+
+        [Required]
         public string Numero { get; set; }
+
+        [Required, StringLength(40, MinimumLength = 3)]
         public string Complemento { get; set; }
-        public string Bairro { get; set; }
+
+        [Required, StringLength(50, MinimumLength = 3)]
+        public string bairro { get; set; }
+
+        [Required,StringLength(60, MinimumLength = 6)]
         public string PontoDeReferencia { get; set; }
     }
 }
--- MedicalCare/Models/EnderecoModel.cs
+++ MedicalCare/Models/EnderecoModel.cs
@@ -9,28 +9,20 @@ namespace MedicalCare.Models
         [Key]
         public int Id { get; set; }
 
-        [Required, StringLength(8, MinimumLength = 8)]
         public string Cep { get; set; }
 
-        [Required, StringLength(30, MinimumLength = 3)]
         public string Cidade { get; set; }
 
-        [Required, StringLength(20, MinimumLength = 2)]
         public string Estado { get; set; }
 
-        [Required, StringLength(50, MinimumLength = 8)]
         public string Logradouro { get; set; }
 
-        [Required]
         public string Numero { get; set; }
 
-        [Required, StringLength(40, MinimumLength = 5)]
         public string Complemento { get; set; }
 
-        [Required, StringLength(50, MinimumLength = 3)]
         public string Bairro { get; set; }
 
-        [Required, StringLength(60, MinimumLength = 6)]
         public string PontoDeReferencia { get; set; }
         public int PacienteId { get; set; }
         public PacienteModel Paciente { get; set; }
