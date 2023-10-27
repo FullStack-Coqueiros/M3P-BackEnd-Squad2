52ce4cc268c0e96050a539715767c6151aa69525
Erico <ericoam.mussulin5@gmail.com>
2023-10-27 15:01:13 -0300

    Exclusão de logsDto desnecessárias.

--- MedicalCare/DTO/LogCreateDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MedicalCare.DTO
-{
-    public class LogCreateDTO
-    {
-        public DateTime Data { get; set; }
-        public string Dominio { get; set; }
-        public string Descricao { get; set; }
-    }
-}
